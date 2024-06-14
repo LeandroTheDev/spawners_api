a89ccd58db8d755e082a302d7e06cf83ab7abae8
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2024-06-14 17:01:39 -0300

    1.0.0-pre.1

---
+++ SpawnersAPI/Initialization.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Server;
+
+namespace SpawnersAPI;
+
+public class Initialization : ModSystem
+{
+    public override void AssetsFinalize(ICoreAPI api)
+    {
+        base.AssetsFinalize(api);
+    }
+
+    public override void AssetsLoaded(ICoreAPI api)
+    {
+        base.AssetsLoaded(api);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+    }
+
+    public override double ExecuteOrder()
+    {
+        return base.ExecuteOrder();
+    }
+
+    public override void Start(ICoreAPI api)
+    {
+        base.Start(api);
+        Debug.LoadLogger(api.Logger);
+        Debug.Log($"Running on Version: {Mod.Info.Version}");
+        #region block-register
+        api.RegisterBlockEntityClass("spawner", typeof(Spawner));
+        #endregion
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        base.StartClientSide(api);
+    }
+
+    public override void StartPre(ICoreAPI api)
+    {
+        base.StartPre(api);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        base.StartServerSide(api);
+    }
+}
+
+public class Debug
+{
+    private static readonly OperatingSystem system = Environment.OSVersion;
+    static private ILogger loggerForNonTerminalUsers;
+
+    static public void LoadLogger(ILogger logger) => loggerForNonTerminalUsers = logger;
+    static public void Log(string message)
+    {
+        // Check if is linux or other based system and if the terminal is active for the logs to be show
+        if ((system.Platform == PlatformID.Unix || system.Platform == PlatformID.Other) && Environment.UserInteractive)
+            // Based terminal users
+            Console.WriteLine($"{DateTime.Now:d.M.yyyy HH:mm:ss} [SpawnersAPI] {message}");
+        else
+            // Unbased non terminal users
+            loggerForNonTerminalUsers?.Log(EnumLogType.Notification, $"[SpawnersAPI] {message}");
+    }
+}
---
+++ SpawnersAPI/Shared/Spawner.cs
@@ -0,0 +1,112 @@
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+
+namespace SpawnersAPI;
+
+class Spawner : BlockEntity
+{
+    static public event Action<Entity> OnSpawnerSpawn;
+    private int progressSpawn = 0;
+    private long progressTickID = 0;
+
+    public override void Initialize(ICoreAPI api)
+    {
+        base.Initialize(api);
+        progressTickID = RegisterGameTickListener(OnTickRate, 2000, 0);
+    }
+
+    private void OnTickRate(float obj)
+    {
+        // Progress calculation based on the light level of the block
+        int lightLevel = Api.World.BlockAccessor.GetLightLevel(Pos, EnumLightLevelType.MaxLight);
+        if (lightLevel <= 5) progressSpawn += 10;
+        else if (lightLevel <= 10) progressSpawn += 5;
+        else if (lightLevel <= 15) progressSpawn += 3;
+        else if (lightLevel <= 25) progressSpawn += 1;
+        else progressSpawn = 0;
+
+        // Check final progress
+        if (progressSpawn >= 100)
+        {
+            SpawnEntities();
+            progressSpawn = 0;
+        }
+    }
+
+    private void SpawnEntities()
+    {
+        float minX = Pos.X - 4;
+        float minY = Pos.Y - 2;
+        float minZ = Pos.Z - 4;
+        float maxX = Pos.X + 4;
+        float maxY = Pos.Y + 2;
+        float maxZ = Pos.Z + 4;
+        byte maxChances = 10;
+        byte entitiesSpawned = 0;
+        Random random = new();
+        for (int i = 0; i < maxChances; i++)
+        {
+            i++;
+            // Get a random block position
+            int spawnX = random.Next((int)minX, (int)maxX);
+            int spawnY = random.Next((int)minY, (int)maxY);
+            int spawnZ = random.Next((int)minZ, (int)maxZ);
+            // Get the block with the random position
+            BlockPos spawnBlockPos = new(spawnX, spawnY, spawnZ, 0);
+            Block spawnBlock = Api.World.BlockAccessor.GetBlock(spawnBlockPos);
+            // Check if block is free
+            if (spawnBlock.Code.ToString() == "game:air")
+            {
+                // Now we need to check the if he has free space upper
+                Block upperBlock = Api.World.BlockAccessor.GetBlock(spawnBlockPos);
+                if (upperBlock.Code.ToString() == "game:air")
+                {
+                    entitiesSpawned++;
+
+                    // Getting Entity info
+                    EntityProperties type = Api.World.GetEntityType(new AssetLocation(Block.Code.ToString().Replace("spawnersapi:spawner-", "")));
+                    // Check if entity exist
+                    if (type == null)
+                    {
+                        Debug.Log($"ERROR: the entity from {Block.Code} is null: {Block.Code.ToString().Replace("spawnersapi:spawner-", "")}");
+                        break;
+                    }
+                    // Instanciating
+                    Entity entity = Api.World.ClassRegistry.CreateEntity(type);
+                    entity.ServerPos.X = spawnX;
+                    entity.ServerPos.Y = spawnY;
+                    entity.ServerPos.Z = spawnZ;
+                    entity.Pos.SetPos(entity.ServerPos);                    
+                    // Spawning
+                    Api.World.SpawnEntity(entity);
+                    OnSpawnerSpawn.Invoke(entity);
+
+                    // Limit of 4 entities spawned at once
+                    if (entitiesSpawned >= 4) break;
+                }
+            }
+        }
+
+    }
+
+    public override void ToTreeAttributes(ITreeAttribute tree)
+    {
+        base.ToTreeAttributes(tree);
+        tree.SetInt("progressSpawn", progressSpawn);
+    }
+
+    public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldForResolving)
+    {
+        base.FromTreeAttributes(tree, worldForResolving);
+        progressSpawn = tree.GetInt("progressSpawn");
+    }
+
+    public override void OnBlockUnloaded()
+    {
+        base.OnBlockUnloaded();
+        UnregisterGameTickListener(progressTickID);
+    }
+}
